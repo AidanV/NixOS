847ffa83963960f6bc217aa6f637e8b1b7609472
AidanV <aidanvanduyne@gmail.com>
2023-11-10 00:19:43 -0800

    moved hardware and home

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@
 
 { imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix ];
+      ./hardware-configuration.nix ];
 
   # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.aidan = import ./home.nix;
+            home-manager.users.aidan = import ./home;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
